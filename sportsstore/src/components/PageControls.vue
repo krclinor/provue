b74b6002b13540b097e9c28a2788204bc7a1ed1c
krclinor <krclinor@gmail.com>
2019-02-02 19:02:22 +0900

    7장 마무리

--- sportsstore/src/components/PageControls.vue
+++ sportsstore/src/components/PageControls.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="row mt-2">
-		<div class="col form-group">
+		<div class="col-3 form-group">
 			<select class="form-control" @change="changePageSize">
 				<option value="4">4 per page</option>
 				<option value="8">8 per page</option>
@@ -8,29 +8,62 @@
 			</select>
 		</div>
 		<div class="text-right col">
-			<div class="btn-group mx-2">
-				<button v-for="i in pageNumbers" :key="i"
-					class="btn"
+			<button class="btn btn-secondary mx-1"
+				:disabled="currentPage == 1"
+				@click="setCurrentPage(currentPage - 1)">
+				Previous
+			</button>
+			<span v-if="currentPage > 4">
+				<button class="btn btn-secondary mx-1"
+					@click="setCurrentPage(1)">
+					1
+				</button>
+				<span class="h4">...</span>
+			</span>
+			<span class="mx-1">
+				<button class="btn"
+					v-for="i in pageNumbers"
+					:key="i"
 					:class="{'btn-primary': i == currentPage, 'btn-secondary': i != currentPage}"
 					@click="setCurrentPage(i)">
 					{{i}}
 				</button>
-			</div>
+			</span>
+			<span v-if="currentPage <= pageCount - 4">
+				<span class="h4">...</span>
+				<button class="btn btn-secondary mx-1"
+					@click="setCurrentPage(pageCount)">
+					{{pageCount}}
+				</button>
+			</span>
+			<button class="btn btn-secondary mx-1"
+				:disabled="currentPage == pageCount"
+				@click="setCurrentPage(currentPage + 1)">
+				Next
+			</button>
 		</div>
 	</div>
 </template>
 <script>
-import {mapState, mapGetters, mapMutations} from "vuex"
+import {mapState, mapGetters, mapActions} from "vuex"
 export default {
 	computed:{
 		...mapState(["currentPage"]),
 		...mapGetters(["pageCount"]),
 		pageNumbers(){
-			return [...Array(this.pageCount + 1).keys()].slice(1)
+			if(this.pageCount < 4){
+				return [...Array(this.pageCount + 1).keys()].slice(1)
+			}else if (this.currentPage <= 4){
+				return [1, 2, 3, 4, 5]
+			}else if ((this.currentPage + 4) > this.pageCount){
+				return [this.pageCount - 4, this.pageCount - 3, this.pageCount - 2, this.pageCount - 1, this.pageCount]
+			}else {
+				return [this.currentPage - 1, this.currentPage, this.currentPage + 1]
+			}
 		}
 	},
 	methods:{
-		...mapMutations(["setCurrentPage", "setPageSize"]),
+		...mapActions(["setCurrentPage", "setPageSize"]),
 		changePageSize($event){
 			this.setPageSize(Number($event.target.value))
 		}
---
+++ sportsstore/src/components/admin/Admin.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="contailer-fluid">
+		<div class="row">
+			<div class="col bg-secondary text-white">
+				<a class="navbar-brand">SPORTS STORE Admin</a>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-3 bg-secondary p-2">
+				<router-link class="btn btn-block btn-primary"
+					to="/admin/products"
+					active-class="active">
+					Products
+				</router-link>
+				<router-link class="btn btn-block btn-primary"
+					to="/admin/orders"
+					active-class="active">
+					Orders
+				</router-link>
+			</div>
+			<div class="col-9 p-2">
+				<router-view/>
+			</div>
+		</div>
+	</div>
+</template>

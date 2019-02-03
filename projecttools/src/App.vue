ed266a339879a466523b2a99755b9010994b28f8
krclinor <krclinor@gmail.com>
2019-02-04 01:14:34 +0900

    10장 Vue.js 프로젝트와 툴 이해하기

---
+++ projecttools/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+	<div id="app">
+		<div>Button Clicks: {{counter}}</div>
+		<button v-on:click="incrementCounter">눌러보세요</button>
+		<div id="nav">
+			<router-link to="/">Home</router-link> |
+			<router-link to="/about">About</router-link>
+		</div>
+		<router-view/>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				counter: 0
+			}
+		},
+		methods: {
+			incrementCounter() {
+				//debugger
+				this.counter++
+			}
+		}
+	}
+</script>
+
+<style>
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+	}
+	
+	#nav {
+		padding: 30px;
+	}
+	
+	#nav a {
+		font-weight: bold;
+		color: #2c3e50;
+	}
+	
+	#nav a.router-link-exact-active {
+		color: #42b983;
+	}
+</style>

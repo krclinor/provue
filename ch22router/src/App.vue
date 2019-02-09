9dd5b3b2c8a0112aeffb6727ecce00cd318862e8
krclinor <krclinor@gmail.com>
2019-02-09 15:58:00 +0900

    22장 route

---
+++ ch22router/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col m-2">
+				<router-view></router-view>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+
+export default {
+	name: "App",
+	created(){
+		this.$store.dispatch("getProductsAction")
+	}	
+}
+</script>
+
